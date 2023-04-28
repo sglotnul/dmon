1f0d8bf1e49e5451ca2d9290c7393ea0d917e760
unknown <329281-7>
2023-04-28 15:39:53 +0300

    dumb table view

---
+++ Forms/Controls/TableView/DumbTableView.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Data;
+using System.Linq;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+using Dmon.Model;
+
+namespace Dmon.Forms.Controls.TableView
+{
+    class DumbTableView : TabPage
+    {
+
+    }
+}
